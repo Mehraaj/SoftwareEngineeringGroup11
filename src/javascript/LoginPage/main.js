9916f2abc20be5cdf52939dd85f55b84eeb3f922
Sahmi Abubakar <sahmi.abubakar@gmail.com>
2022-11-03 18:42:59 -0400

    started working on loginpage and worked on product listpage

--- src/javascript/LoginPage/main.js
+++ src/javascript/LoginPage/main.js
@@ -12,17 +12,3 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
-
-let ATC = document.getElementsByName("ATC");
-let Counter = document.getElementById("ATCcounter")
-
-var CartCounter = 0
-for(let i =0; i<ATC.length;i++){
-  ATC[i].onclick = function(){
-
-  CartCounter++;
-  Counter.style.transition = "all 0.3s ease 0s";
-  Counter.style.backgroundColor = "red";
-  Counter.textContent = CartCounter.toString();
-}
-}
